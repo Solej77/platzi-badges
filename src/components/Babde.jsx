1de471163c86398bf0988a2489642e0616f4167b
Solej77 <ing.josgalo@gmail.com>
2020-03-12 13:42:26 -0600

    How apply styles

--- src/components/Babde.jsx
+++ src/components/Babde.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 
+import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia"/>
         </div>
 
-        <div>
-          <img src="" alt="Avatar"/>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://s.gravatar.com/avatar/6371f4360f3fd7260fce4b90deebbe73?s=80" alt="Avatar"/>
           <h1>José Luis <br/> García</h1>
         </div>
 
-        <div>
-          <p>Software Engineer</p>
-          <p>@Solej77</p>
+        <div className="Badge__section-info">
+          <h3>Software Engineer</h3>
+          <div>@Solej77</div>
         </div>
 
-        <div>
+        <div className="Badge__footer">
           #platziconf
         </div>
-      </>
+      </div>
     )
   }
 }
