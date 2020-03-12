10c62ea7d68e6539f655dcfb0eb26df3329e4038
Solej77 <ing.josgalo@gmail.com>
2020-03-12 16:03:32 -0600

    Props

--- src/components/Babde.jsx
+++ src/components/Babde.jsx
@@ -4,6 +4,7 @@ import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
+
   render() {
     return (
       <div className="Badge">
@@ -12,13 +13,13 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img className="Badge__avatar" src="https://s.gravatar.com/avatar/6371f4360f3fd7260fce4b90deebbe73?s=80" alt="Avatar"/>
-          <h1>José Luis <br/> García</h1>
+          <img className="Badge__avatar" src={this.props.avatar} alt="Avatar"/>
+          <h1>{this.props.firstName} <br/> {this.props.lastName}</h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Software Engineer</h3>
-          <div>@Solej77</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">
--- src/index.js
+++ src/index.js
@@ -7,4 +7,11 @@ import "./global.css";
 import Badge from "./components/Babde";
 
 const app = document.getElementById("app");
-ReactDOM.render(<Badge />, app);
+ReactDOM.render(
+  <Badge 
+    firstName="José Luis"
+    lastName ="García"
+    jobTitle="Software Engieneer"
+    twitter="Solej77"
+    avatar="https://s.gravatar.com/avatar/6371f4360f3fd7260fce4b90deebbe73?s=80"
+  />, app);
