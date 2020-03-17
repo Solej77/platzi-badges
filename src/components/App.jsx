f8c5757b7496bc2fa9b8441ba5a744021da83860
Solej77 <ing.josgalo@gmail.com>
2020-03-17 08:20:40 -0600

    Add Home and NotFound components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Layout from "./Layout";
+import Home from "../pages/Home";
 import Badges from "../pages/Badges";
 import BadgeNew from "../pages/BadgeNew";
 import BadgeDetailsContainer from "../pages/BadgeDetailsContainer";
@@ -13,6 +14,7 @@ function App () {
     <BrowserRouter>
     <Layout>
       <Switch>
+      <Route exact path="/" component={Home} />
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
         <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom"
+
+import "./styles/NotFound.css";
 
 function NotFound() {
-  return "Not Found";
+  return (
+    <div className="NotFound">
+      <div className="NotFound__container">
+      <div className="row">
+        <div className="col-2">
+
+        </div>
+        <div className="col-8">
+          <h1>Error 404:</h1>
+          <Link className="btn btn-primary" to="/badges">
+            Start
+          </Link>
+        </div>
+      </div>
+  
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
